267286d786c96701ade36dc790c6c91d4165964e
Val <valentin.podda@etu.unice.fr>
2022-11-07 16:50:46 +0100

    Added a file generator

---
+++ src/simulator/FileGenerator.java
@@ -0,0 +1,28 @@
+package simulator;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileGenerator {
+
+    static final int WIDTH = 100;
+    static final int HEIGHT = 100;
+    static final int nbPersons = 100;
+
+    public static void main(String... args) {
+        try {
+            String str = WIDTH + " " + HEIGHT;
+            BufferedWriter writer = new BufferedWriter(new FileWriter("initialisation_file.txt"));
+            writer.write(str);
+            for(int i = 0; i<nbPersons; i++){
+                str = "\n" + (int) (Math.random() * (WIDTH)) + " " + (int) (Math.random() * (HEIGHT)) + " " + (int) (Math.random()*(WIDTH)) + " " + (int) (Math.random() * (HEIGHT));
+                writer.append(str);
+            }
+            writer.close();
+        }
+        catch(IOException e) {
+            System.out.print(e);
+        }
+    }
+}
